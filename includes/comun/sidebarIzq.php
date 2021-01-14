0c3739b90367421eb6323abcf83e0687bde3cc8c
gonzalofdv <gfigue01@ucm.es>
2021-01-14 23:24:07 +0100

    Web0.9

--- includes/comun/sidebarIzq.php
+++ includes/comun/sidebarIzq.php
@@ -1,15 +1,14 @@
-<div id="sidebar-left">
+<div style='font-size: 23px;' id="sidebar-left">
 	<ul>
-		<ul><a href="index.php">Inicio</a></ul>
-		<ul><a>Seguidores: </a><?php 
+		<ul style='font-size: 30px;'>Seguidores: <?php 
 		if(isset($_SESSION['seguidores'])) echo $_SESSION['seguidores'];
 		else echo "0"; 
 		?>
 		</a></ul>
 		<ul><button id="button-subir-foto" onclick=location.href="accionSubirFoto.php" style='font-size: 20px; width:160px; height:60px'>Subir foto</button></ul>
 	</ul>
-	<div id="publi">
-		<h2>Espacio publicitario</h2>
+	<div style='font-size: 23px;' id="publi">
+		<h2 style='font-size: 30px;'>Espacio publicitario</h2>
 		Compra el nuevo coso que te hara feliz y mas popular!!!
 	</div>
 	<!--aqui poner movidas de publi-->
